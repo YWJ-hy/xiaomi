20d6694609d176314144c0f05c9c2e0b7c5fb5e2
YWJ <446460840@qq.com>
2019-04-15 11:00:38 +0800

    引入vant

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+import { Search } from 'vant'
+Vue.use(Search)
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
---
+++ src/views/home/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="home_container">
+    <div class="home_header">
+      <img src="/static/home/下载.png" alt="">
+      <van-search placeholder="请输入搜索关键词" v-model="value" />
+    </div>
+  </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style lang="scss">
+
+</style>
